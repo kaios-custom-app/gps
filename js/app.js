55b8a3f0bd6197c364d0fdf0e5a2fbfef8def7ab
thanh <20144075@student.hust.edu.vn>
2020-08-19 21:19:57 +0900

    sucessfully extract lat and lon

--- js/app.js
+++ js/app.js
@@ -1,29 +1,26 @@
-// DOMContentLoaded is fired once the document has been loaded and parsed,
-// but without waiting for other external resources to load (css/images/etc)
-// That makes the app more responsive and perceived as faster.
-// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
 window.addEventListener('DOMContentLoaded', function() {
 
-  // We'll ask the browser to use strict code to help us catch errors earlier.
-  // https://developer.mozilla.org/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
   'use strict';
 
-  var translate = navigator.mozL10n.get;
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 60000,
+      maximumAge: 0
+    };
 
-  // We want to wait until the localisations library has loaded all the strings.
-  // So we'll tell it to let us know once it's ready.
-  navigator.mozL10n.once(start);
+    function success(pos) {
+      var crd = pos.coords;
 
-  // ---
+      document.write('Your current position is:\n');
+      document.write(`Latitude : ${crd.latitude}\n`);
+      document.write(`Longitude: ${crd.longitude}\n`);
+      document.write(`More or less ${crd.accuracy} meters.\n`);
+    }
 
-  function start() {
+    function error(err) {
+      document.write(`ERROR(${err.code}): ${err.message}`);
+    }
 
-    var message = document.getElementById('message');
-
-    // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
-    // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
-    message.textContent = translate('message');
-
-  }
+    navigator.geolocation.getCurrentPosition(success, error, options);
 
 });
